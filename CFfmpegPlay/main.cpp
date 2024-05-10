195a5ecc617d190575932c387053b42c4ccb6a71
crudapi <admin@crudapi.cn>
2024-05-10 14:42:24 +0800

    av_version_info

--- CFfmpegPlay/main.cpp
+++ CFfmpegPlay/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    qDebug() << "ffmpeg信息: " << av_version_info();
     return a.exec();
 }
